36da0dec64a4b647373404b6f2e986c59800619c
Neel Joshi <nbjoshi@unc.edu>
2024-08-01 01:11:03 -0400

    Day One: Front End Complete

---
+++ script.js
@@ -0,0 +1,7 @@
+const color = document.querySelector("#colorinput");
+const rainbow = document.querySelector("#rainbowinput");
+const eraser = document.querySelector("#eraserinput");
+const gridInput = document.querySelector("#gridrange");
+const gridText = document.querySelector("#gridtext");
+const clear = document.querySelector("#clearinput");
+const container = document.querySelector(".container");
